db23295a4f3770b232c96d5c5032cf7753ab65a2
sup <25865696+kkss2020@users.noreply.github.com>
2020-04-13 14:39:06 +0800

    Update custom.sh

--- user/default/custom.sh
+++ user/default/custom.sh
@@ -6,6 +6,3 @@ git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverc
 
 ./scripts/feeds update -a
 ./scripts/feeds install -a
-
-sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
-
