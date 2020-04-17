1de852c68074e3aa735d2872274d436d568aa42e
sup <25865696+kkss2020@users.noreply.github.com>
2020-04-17 11:00:50 +0800

    Update custom.sh

--- user/default/custom.sh
+++ user/default/custom.sh
@@ -3,6 +3,6 @@
 echo "Test custom.sh"
 
 git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
-
+git clone https://github.com/fw876/helloworld/tree/master/luci-app-ssr-plus
 ./scripts/feeds update -a
 ./scripts/feeds install -a
