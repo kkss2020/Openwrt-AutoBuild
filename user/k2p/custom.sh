446bee220bb78af72989ed1c5dc0b9209c4e7d4a
sup <25865696+kkss2020@users.noreply.github.com>
2021-01-27 18:12:58 +0800

    Update custom.sh

--- user/k2p/custom.sh
+++ user/k2p/custom.sh
@@ -5,5 +5,6 @@ echo "Test custom.sh"
 git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
 git clone https://github.com/fw876/helloworld package/ssr
 git clone https://github.com/rufengsuixing/luci-app-adguardhome                          package/adg
+git clone https://github.com/jerrykuku/luci-app-jd-dailybonus package/jd
 ./scripts/feeds update -a
 ./scripts/feeds install -a
